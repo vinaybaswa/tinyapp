9ca1b8a4a9927055491e0b1cfc11fa62b741417e
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-31 21:41:44 +0000

    addet route /urls/new before /urls/:...

--- express_server.js
+++ express_server.js
@@ -25,21 +25,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
- app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
- });
+});
 
- app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
